e008f974ba7b39e08b35a7bb68eb796ac2526286
tannerpace <52814471+tannerpace@users.noreply.github.com>
2023-04-18 23:23:38 -0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,156 @@
+package com.example.myapplication
+
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+    private val cameraActivityResultContract = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK) {
+            // TODO: Handle the photo result
+        }
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun displayNotification() {
+        val notificationId = 1
+        val notificationChannelId = "MyChannelId"
+
+        // Create an intent for the notification to open when clicked
+        val intent = Intent(this, MyActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        // Build the notification
+        val notificationBuilder = NotificationCompat.Builder(this, notificationChannelId)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle("My Notification")
+            .setContentText("This is a notification.")
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        // Get the NotificationManager
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        // Check if the Android version is Oreo or higher, and create a notification channel
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(notificationChannelId, "My Channel", NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        // Display the notification
+        notificationManager.notify(notificationId, notificationBuilder.build())
+    }
+
+    class MyActivity {
+
+    }
+
+    private fun startCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraActivityResultContract.launch(intent)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                // Handle settings item click
+                true
+            }
+            R.id.action_camera -> {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    val PERMISSION_CODE = 123
+                    ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), PERMISSION_CODE)
+                } else {
+                    startCamera()
+                }
+
+                true
+            }
+            R.id.notification_background -> {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    // Create a notification channel for Android Oreo or higher
+                    val name = "My Notification Channel"
+                    val descriptionText = "This is my notification channel"
+                    val importance = NotificationManager.IMPORTANCE_DEFAULT
+                    val channel = NotificationChannel("my_channel_id", name, importance).apply {
+                        description = descriptionText
+                    }
+                    val notificationManager = getSystemService(NotificationManager::class.java)
+                    notificationManager.createNotificationChannel(channel)
+                }
+
+                // Create a notification builder
+                val builder = NotificationCompat.Builder(this, "my_channel_id")
+                    .setSmallIcon(R.drawable.ic_notification)
+                    .setContentTitle("My Notification")
+                    .setContentText("This is a background notification.")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setAutoCancel(true)
+
+                // Show the notification
+                val notificationManager = NotificationManagerCompat.from(this)
+                notificationManager.notify(1, builder.build())
+
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
